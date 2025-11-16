06224c1466732c8c38293fa7e83479e7d95ea398
lukaivanovic <lluka.ivanovic@gmail.com>
2025-11-16 16:53:28 +0100

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,7 +68,7 @@ export default function RootLayout({
         </header>
         <main className="bg-background flex flex-col items-start md:flex-row justify-between gap-y-20 md:gap-y-6 md:gap-x-30 text-body container mt-22">
           <div className="md:sticky md:top-20 flex flex-col gap-6 w-full md:w-64 shrink-0">
-            <div className="flex flex-col">
+            <div className="flex flex-col w-50">
               <div className="flex flex-row justify-between text-caption uppercase">
                 <span>Luka Ivanovic</span>
                 <span>I.S.P.</span>
@@ -94,7 +94,7 @@ export default function RootLayout({
           </div>
           <div>{children}</div>
         </main>
-        <div className="h-4 bg-background fixed bottom-0 left-0 right-0 z-10 opacity-0"></div>
+        <div className="h-3 bg-background fixed bottom-0 left-0 right-0 z-10 opacity-[0.01]"></div>
         <Analytics />
         <SpeedInsights />
       </body>
