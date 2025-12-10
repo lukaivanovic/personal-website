f31e2a1086bc7e32dc890a147b0ff08b6077dd82
lukaivanovic <lluka.ivanovic@gmail.com>
2025-12-10 08:31:22 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
-import createMDX from "@next/mdx";
-
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-});
 
 const nextConfig: NextConfig = {
   output: "export",
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 };
 
-export default withMDX(nextConfig);
+export default nextConfig;
