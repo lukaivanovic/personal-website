49de8710e43a92cec62d899f86d441dcacd641f0
lukaivanovic <lluka.ivanovic@gmail.com>
2026-01-06 11:26:16 +0100

    v0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,91 +49,17 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="vanilla" className="[scrollbar-gutter:stable]">
       <body className="overflow-x-hidden">
-        <ShaderBackgroundWrapper />
-
-        <main className="grid grid-cols-1 md:grid-cols-[560px_1fr]">
-          <header className="text-body">
-            <div className="flex flex-col md:sticky md:top-0 md:min-h-screen pt-4 pb-4 px-6">
-              <div className="flex flex-row items-center justify-between gap-2">
-                <Link href="/" className="flex flex-row items-center gap-2">
-                  <Apollon className="size-17" />
-                </Link>
-              </div>
-
-              <h1 className="text-[28px] leading-[36px] font-medium text-pretty mt-14">
-                Independent design engineer helping companies achieve design
-                excellence.
-              </h1>
-              <p className="text-foreground/72 mt-2">
-                Living and working out of Zagreb, Croatia.
-              </p>
-
-              <div className="flex flex-col py-10">
-                <div className="relative grid grid-cols-[96px_1fr_80px] items-center h-9 gap-2 w-full border-b border-foreground/12">
-                  <span className="opacity-50">Client</span>
-                  <span className="opacity-50">Description</span>
-                  <span className="opacity-50">Period</span>
+        <main className="container">
+          <header className="py-3">
+            <div className="flex flex-row items-center justify-between gap-2">
+              {/* <Link href="/" className="flex flex-row items-center gap-2">
+                <Apollon className="size-15" />
+              </Link> */}
+              <div className="text-[13px]">
+                <div className="font-medium">Luka Ivanovic D.E.P</div>
+                <div className="text-secondary">
+                  Design Engineering Practice
                 </div>
-                <div className="flex flex-col">
-                  <ProjectRow
-                    name="Profound"
-                    description="GenAI marketing intelligence platform"
-                    period="2025"
-                    href="https://tryprofound.com"
-                  />
-                  <ProjectRow
-                    name="Daytona"
-                    description="Sandboxes for running AI-generated code"
-                    period="2025"
-                    href="https://daytona.io"
-                  />
-
-                  <ProjectRow
-                    name="Buena"
-                    description="Property management firm"
-                    period="2025"
-                    href="https://buena.com"
-                  />
-                  <ProjectRow
-                    name="Akkio"
-                    description="AI agents for media agencies"
-                    period="2024"
-                    href="https://weweb.io"
-                  />
-                  <ProjectRow
-                    name="WeWeb"
-                    description="No-code AI application builder"
-                    period="2022-2025"
-                    href="https://weweb.io"
-                  />
-                </div>
-              </div>
-
-              <div className="flex flex-col items-center gap-2 h-9 w-full md:w-auto">
-                <a
-                  href="mailto:hi@ivanovicluka.co"
-                  className="px-3 w-full py-1 bg-foreground flex items-center justify-center text-background border border-transparent hover:border-foreground hover:bg-background hover:text-foreground cursor-pointer md:cursor-default "
-                >
-                  Contact
-                </a>
-                <div className="flex flex-row items-center gap-2">
-                  <a
-                    href="https://x.com/lukaivnvc"
-                    className="px-2 py-0.5 text-secondary hover:text-foreground  cursor-pointer md:cursor-default "
-                  >
-                    TW
-                  </a>
-                  <a
-                    href="https://www.linkedin.com/in/ivanovicluka0/"
-                    className="px-2 py-0.5 text-secondary hover:text-foreground  cursor-pointer md:cursor-default "
-                  >
-                    LI
-                  </a>
-                </div>
-              </div>
-
-              <div className="mt-auto">
-                <ThemeSelect />
               </div>
             </div>
           </header>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,138 +1,251 @@
 import VideoPlayer from "@/components/video-player/VideoPlayer";
 import VideoWithCaption from "@/components/video-player/VideoWithCaption";
+import ProfoundLogo from "@/assets/ProfoundLogo";
+import DaytonaLogo from "@/assets/DaytonaLogo";
+import TurboLogo from "@/assets/TurboLogo";
+import BuenaLogo from "@/assets/BuenaLogo";
+import WewebLogo from "@/assets/WewebLogo";
+import AkkioLogo from "@/assets/AkkioLogo";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col gap-28 p-4">
-      {/* Profound Shopping */}
-      <section className="flex flex-col items-center gap-4">
-        <div className="aspect-3/2 relative px-6 rounded-xl flex items-center justify-center">
-          <img
-            src="/profound-3.png"
-            alt=""
-            className="z-10 w-full h-auto object-cover scale-90"
-            width={1200}
-            height={800}
-          />
-          <img
-            src="/profound-bg.png"
-            alt="Profound Shopping background"
-            className="w-full h-full absolute inset-0 object-cover"
-          />
+    <>
+      <section className="pt-24 pb-24">
+        <h1 className="title max-w-[800px] pb-2">
+          Design engineering practice founded by Luka Ivanovic. Combining design
+          and development into one discipline, the practice reduces handoffs,
+          minimizes rework, and enables teams to ship with clarity and speed.
+          Built for founders who appreciate craft and want to stay lean without
+          compromising quality.
+        </h1>
+        <p className="text-secondary pb-8 max-w-[800px]">
+          We offer continuous personal relationship depending on your needs
+          instead of limited subscription packages or sprints.
+        </p>
+
+        <div className="flex flex-row items-center gap-3 pb-12 text-[13px]">
+          <a
+            className="px-4 py-2 bg-foreground text-background rounded-full"
+            href="https://wa.me/385958845960"
+            target="_blank"
+          >
+            Chat with me (WhatsApp)
+          </a>
+          <span className="text-secondary">
+            {" "}
+            or email at{" "}
+            <a href="mailto:hi@lukaivanovic.co" className=" text-foreground">
+              hi@lukaivanovic.co
+            </a>
+            .
+          </span>
         </div>
-        <div className="flex flex-col items-center text-center w-120 gap-1 ">
-          <h2 className="font-medium">Profound Shopping</h2>
-          <p className="text-secondary">
-            Built the brand new Profound Shopping analytics package which
-            provides an extremely detailed view into ChatGPT's shopping results.
-          </p>
+
+        <div className="h-px bg-foreground/6 w-full" />
+
+        <div className="flex flex-row items-center gap-4 text-secondary py-6">
+          <div className="text-secondary text-[13px]">Trusted by</div>
+
+          <div className="flex flex-row gap-4">
+            <ProfoundLogo className="h-5 w-auto" />
+            <DaytonaLogo className="h-5 w-auto" />
+            {/* <TurboLogo className="h-7 w-auto" /> */}
+            <BuenaLogo className="h-5 w-auto" />
+            <WewebLogo className="h-5 w-auto" />
+            <AkkioLogo className="h-5 w-auto" />
+          </div>
+        </div>
+
+        <div className="h-px bg-foreground/6 w-full" />
+
+        <div className="grid grid-cols-[repeat(4,140px)] text-[13px] pb-6 pt-6">
+          <div className="flex flex-col gap-3">
+            <div className="text-secondary">Design</div>
+
+            <ul>
+              <li>Web</li>
+              <li>Mobile</li>
+              <li>Desktop</li>
+              <li>Websites</li>
+              <li>Branding</li>
+            </ul>
+          </div>
+
+          <div className="flex flex-col gap-3">
+            <div className="text-secondary">Product</div>
+
+            <ul>
+              <li>User experience</li>
+              <li>Strategy</li>
+              <li>Research</li>
+            </ul>
+          </div>
+
+          <div className="flex flex-col gap-3">
+            <div className="text-secondary">Development</div>
+
+            <ul>
+              <li>Frontend</li>
+              <li>iOS</li>
+              <li>Creative coding</li>
+              <li>Desktop</li>
+              <li>Design systems</li>
+            </ul>
+          </div>
+
+          <div className="flex flex-col gap-3">
+            <div className="text-secondary">Technologies</div>
+
+            <ul>
+              <li>React</li>
+              <li>Solid</li>
+              <li>Next.js</li>
+              <li>Tailwind</li>
+              <li>Radix & Base UI</li>
+              <li>SwiftUI</li>
+              <li>Motion.dev</li>
+              <li>Three.js</li>
+              <li className="text-secondary">+ many more</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="h-px bg-foreground/6 w-full" />
+      </section>
+
+      <section className="gap-4 py-24">
+        <h2 className="title pb-1">
+          Building Profound's brand new AI shopping analytics experience
+        </h2>
+        <p className="text-secondary">
+          Profound helps brands get mentioned in AI responses. We worked on
+          brand new analytic pages for shopping responses.
+        </p>
+
+        <div className="grid grid-cols-2 gap-2 mt-6">
+          <div className="aspect-square relative rounded-md  w-full bg-black/6 p-6 flex items-center justify-center">
+            <img src="/profound-2.webp" className="max-w-[95%] h-auto z-10" />
+          </div>
+          <div className="aspect-square relative rounded-md   w-full bg-black/6 p-6 flex items-center justify-center">
+            <img src="/profound-3.png" className="max-w-[95%] h-auto z-10" />
+          </div>
+          <div className="aspect-square relative rounded-md   w-full bg-black/6 p-6 flex items-center justify-center">
+            <img src="/profound-4.png" className="max-w-[95%] h-auto z-10" />
+          </div>
+          <div className="aspect-square relative rounded-md   w-full bg-black/6 p-6 flex items-center justify-center">
+            <video
+              src="/profound-tabs.mp4"
+              className="max-w-[95%] h-auto z-10"
+              autoPlay
+              muted
+              loop
+            />
+          </div>
         </div>
       </section>
 
-      {/* Buena website */}
-      <section className="flex flex-col items-center gap-4">
-        <div className="aspect-3/ w-full bg-linear-to-b from-foreground/10 to-foreground/5 p-6 flex items-center justify-center">
-          <VideoPlayer url="buena" aspectRatio="16/9" className="w-full" />
-        </div>
-        <div className="flex flex-col items-center text-center w-120">
-          <h2 className="font-medium">Buena website</h2>
-          <p className="text-secondary">
-            Built and designed the new buena.com website celebrating their $58M
-            Series A.
-          </p>
+      <section className="gap-4 py-24">
+        <h2 className="title pb-1">
+          Designing and building the most advanced no-code and AI web
+          application builder.
+        </h2>
+        <p className="text-secondary">
+          Daytona is building a new primitive for the AI age — sandbox. We
+          helped them shape their main sandbox screens in their
+          Next.js/Tailwind/Radix stack.
+        </p>
+
+        <div className="grid grid-cols-2 gap-2 mt-6">
+          <div className="aspect-square relative rounded-md w-full bg-black/6 p-6 flex items-center justify-center">
+            <img
+              src="/weweb/editor-new.png"
+              className="max-w-[95%] h-auto z-10"
+            />
+          </div>
+          <div className="aspect-square relative rounded-md w-full bg-black/6 p-6 flex items-center justify-center">
+            <img
+              src="/weweb/edition-panel.png"
+              className="max-w-[95%] h-auto z-10"
+            />
+          </div>
+          <div className="aspect-square relative rounded-md w-full bg-black/6 p-6 flex items-center justify-center">
+            <img
+              src="/weweb/workflows.png"
+              className="max-w-[95%] h-auto z-10"
+            />
+          </div>
+
+          <div className="aspect-square relative rounded-md w-full bg-black/6 p-6 flex items-center justify-center">
+            <VideoPlayer
+              url="ww-ai"
+              aspectRatio="16/10"
+              className="max-h-[60%] w-auto z-10"
+            />
+          </div>
         </div>
       </section>
 
-      {/* WeWeb Editor */}
-      <section className="flex flex-col items-center gap-4">
-        <div className="flex flex-col items-center gap-2">
-          <img
-            src="/weweb/editor-new.png"
-            alt="Create states, components or edit your elements visually"
-            className="pointer-events-none select-none w-full h-auto"
-            width={1200}
-            height={800}
-          />
+      <section className="gap-4 py-24">
+        <h2 className="title pb-1">
+          Landing page for Buena celebrating their $58M Series A.
+        </h2>
+        <p className="text-secondary">
+          We built and designed the new buena.com website celebrating their $58M
+          Series A.
+        </p>
 
-          <img
-            src="/weweb/workflows.png"
-            alt="Manage your app logic and data from a single place"
-            className="pointer-events-none select-none w-full h-auto"
-            width={1200}
-            height={800}
-          />
-          <VideoPlayer
-            url="ww-ai"
-            aspectRatio="16/10"
-            className="pointer-events-none"
-          />
-        </div>
-        <div className="flex flex-col items-center text-center w-120">
-          <h2 className="font-medium">WeWeb Editor</h2>
-          <p className="text-secondary">
-            Platform for building complex web applications with database
-            conntections, comprehensive styling and workflow automation
-          </p>
+        <div className="grid grid-cols-2 gap-2 mt-6">
+          <div className="aspect-video relative rounded-md  w-full bg-black/6 p-6 flex items-center justify-center col-span-2">
+            <VideoPlayer
+              url="buena"
+              aspectRatio="16/9"
+              className="max-h-[80%] w-auto z-10"
+            />
+          </div>
         </div>
       </section>
 
-      {/* Playground */}
-      <section className="flex flex-col gap-6 md:col-span-2">
-        {/* <div className="flex flex-col gap-0.5">
-          <h2 className="text-base font-medium text-pretty">Playground</h2>
-          <p className="text-secondary text-pretty">
-            This is where you will find some of my experiments and prototypes.
-          </p>
+      <section className="gap-4 py-24">
+        <h2 className="title pb-1">
+          Designing and building Daytona’s sandboxes views
+        </h2>
+        <p className="text-secondary">
+          Daytona is building a new primitive for the AI age — sandbox. We
+          helped them shape their main sandbox screens in their
+          Next.js/Tailwind/Radix stack.
+        </p>
+
+        <div className="grid grid-cols-2 gap-2 mt-6">
+          <div className="aspect-video relative rounded-md  w-full bg-black/6 p-6 flex items-center justify-center col-span-2">
+            <img src="/daytona-01.png" className="max-h-[80%] w-auto z-10" />
+          </div>
+          <div className="aspect-square relative rounded-md   w-full bg-black/6 p-6 flex items-center justify-center">
+            <img src="/daytona-02.png" className="max-w-[80%] h-auto z-10" />
+          </div>
+          <div className="aspect-square relative rounded-md   w-full bg-black/6 p-6 flex items-center justify-center">
+            <img src="/daytona-03.png" className="max-w-[80%] h-auto z-10" />
+          </div>
+        </div>
+      </section>
+
+      <section className="py-24 flex flex-col items-center justify-center">
+        {/* <div className="text-[23px] font-medium leading-[32px] text-pretty pb-1">
+          Get in touch
         </div> */}
-        <div className="gap-2 grid grid-cols-1  md:grid-cols-3">
-          <div className="flex flex-col gap-2">
-            <VideoWithCaption
-              url="buena-sidebar-icons"
-              aspectRatio="1/1"
-              caption="Sidebar with animated icons built in Rive for Buena."
-            />
-            <VideoWithCaption
-              url="radial"
-              aspectRatio="5/4"
-              caption="Radial menu concept for a design tool built in React"
-            />
-            <VideoWithCaption
-              url="tsushima"
-              aspectRatio="1/1"
-              caption="Particle dissolve effect inspired by the Ghost of Tsushima game and built with three.js with no external libraries."
-            />
-          </div>
-          <div className="flex flex-col gap-2">
-            <VideoWithCaption
-              url="menu"
-              aspectRatio="5/4"
-              caption="A different take on the animation of an opening submenu where the menu items are revealed from the center instead of the top."
-            />
-            <VideoWithCaption
-              url="rive"
-              aspectRatio="16/9"
-              caption="Rive interactive animation experiment"
-            />
-            <VideoWithCaption
-              url="ios-voice"
-              aspectRatio="16/9"
-              caption="iOS voice command quick feature"
-            />
-          </div>
-          <div className="flex flex-col gap-2">
-            <VideoWithCaption
-              url="gymmy"
-              aspectRatio="1/1"
-              caption="iOS application for tracking your workouts using voice as an input. Built with SwiftUI and LiveKit."
-            />
-            <VideoWithCaption
-              url="material-maker"
-              aspectRatio="1/1"
-              caption="Cool glowing effect built in Material Maker"
-            />
-          </div>
+        <div className="flex flex-row items-center gap-3 pb-8 text-[13px]">
+          <a className="px-4 py-2 bg-foreground text-background rounded-full">
+            Chat with me (WhatsApp)
+          </a>
+          <span className="text-secondary">
+            {" "}
+            or email at{" "}
+            <a href="mailto:hi@lukaivanovic.co" className=" text-foreground">
+              hi@lukaivanovic.co
+            </a>
+            .
+          </span>
         </div>
       </section>
-    </div>
+    </>
   );
 }
