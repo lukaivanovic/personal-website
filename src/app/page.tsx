26a1a3553e409bf275b7bcdc8429fc49a825f7c5
lukaivanovic <lluka.ivanovic@gmail.com>
2026-01-06 11:30:19 +0100

    v01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,12 +29,12 @@ export default function Home() {
             href="https://wa.me/385958845960"
             target="_blank"
           >
-            Chat with me (WhatsApp)
+            Chat with us
           </a>
           <span className="text-secondary">
             {" "}
             or email at{" "}
-            <a href="mailto:hi@lukaivanovic.co" className=" text-foreground">
+            <a href="mailto:hi@lukaivanovic.co" className="text-foreground">
               hi@lukaivanovic.co
             </a>
             .
@@ -122,7 +122,7 @@ export default function Home() {
           brand new analytic pages for shopping responses.
         </p>
 
-        <div className="grid grid-cols-2 gap-2 mt-6">
+        <div className="grid grid-cols-2 gap-2 mt-8">
           <div className="aspect-square relative rounded-md  w-full bg-black/6 p-6 flex items-center justify-center">
             <img src="/profound-2.webp" className="max-w-[95%] h-auto z-10" />
           </div>
@@ -149,13 +149,8 @@ export default function Home() {
           Designing and building the most advanced no-code and AI web
           application builder.
         </h2>
-        <p className="text-secondary">
-          Daytona is building a new primitive for the AI age — sandbox. We
-          helped them shape their main sandbox screens in their
-          Next.js/Tailwind/Radix stack.
-        </p>
 
-        <div className="grid grid-cols-2 gap-2 mt-6">
+        <div className="grid grid-cols-2 gap-2 mt-8">
           <div className="aspect-square relative rounded-md w-full bg-black/6 p-6 flex items-center justify-center">
             <img
               src="/weweb/editor-new.png"
@@ -194,7 +189,7 @@ export default function Home() {
           Series A.
         </p>
 
-        <div className="grid grid-cols-2 gap-2 mt-6">
+        <div className="grid grid-cols-2 gap-2 mt-8">
           <div className="aspect-video relative rounded-md  w-full bg-black/6 p-6 flex items-center justify-center col-span-2">
             <VideoPlayer
               url="buena"
@@ -215,7 +210,7 @@ export default function Home() {
           Next.js/Tailwind/Radix stack.
         </p>
 
-        <div className="grid grid-cols-2 gap-2 mt-6">
+        <div className="grid grid-cols-2 gap-2 mt-8">
           <div className="aspect-video relative rounded-md  w-full bg-black/6 p-6 flex items-center justify-center col-span-2">
             <img src="/daytona-01.png" className="max-h-[80%] w-auto z-10" />
           </div>
@@ -228,13 +223,68 @@ export default function Home() {
         </div>
       </section>
 
+      <section className="flex flex-col gap-6 md:col-span-2">
+        <h2 className="title pb-1">
+          Constantly exploring new ideas and building prototypes
+        </h2>
+
+        <div className="gap-2 grid grid-cols-1  md:grid-cols-3">
+          <div className="flex flex-col gap-2">
+            <VideoWithCaption
+              url="buena-sidebar-icons"
+              aspectRatio="1/1"
+              caption="Sidebar with animated icons built in Rive for Buena."
+            />
+            <VideoWithCaption
+              url="radial"
+              aspectRatio="5/4"
+              caption="Radial menu concept for a design tool built in React"
+            />
+            <VideoWithCaption
+              url="tsushima"
+              aspectRatio="1/1"
+              caption="Particle dissolve effect inspired by the Ghost of Tsushima game and built with three.js with no external libraries."
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <VideoWithCaption
+              url="menu"
+              aspectRatio="5/4"
+              caption="A different take on the animation of an opening submenu where the menu items are revealed from the center instead of the top."
+            />
+            <VideoWithCaption
+              url="rive"
+              aspectRatio="16/9"
+              caption="Rive interactive animation experiment"
+            />
+            <VideoWithCaption
+              url="ios-voice"
+              aspectRatio="16/9"
+              caption="iOS voice command quick feature"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <VideoWithCaption
+              url="gymmy"
+              aspectRatio="1/1"
+              caption="iOS application for tracking your workouts using voice as an input. Built with SwiftUI and LiveKit."
+            />
+            <VideoWithCaption
+              url="material-maker"
+              aspectRatio="1/1"
+              caption="Cool glowing effect built in Material Maker"
+            />
+          </div>
+        </div>
+      </section>
+
       <section className="py-24 flex flex-col items-center justify-center">
         {/* <div className="text-[23px] font-medium leading-[32px] text-pretty pb-1">
           Get in touch
         </div> */}
         <div className="flex flex-row items-center gap-3 pb-8 text-[13px]">
           <a className="px-4 py-2 bg-foreground text-background rounded-full">
-            Chat with me (WhatsApp)
+            Chat with us
           </a>
           <span className="text-secondary">
             {" "}
