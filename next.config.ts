2cfdc684c051101971246acb0fc9dea5e85a2143
luka <lluka.ivanovic@gmail.com>
2026-03-25 15:12:47 +0100

    t

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // output: "export",
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-  async rewrites() {
-    return [
-        {
-            source: '/gradient',
-            destination: `https://ivanovicluka.co/gradient`,
-        }
-    ];
-  }
 };
 
 
