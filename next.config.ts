439a17523702d3a86b087fc5cb623e36157e0f04
luka <lluka.ivanovic@gmail.com>
2026-03-25 15:03:46 +0100

    test

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  // output: "export",
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   async rewrites() {
     return [
         {
             source: '/gradient',
-            destination: `https://www.2026-03-23-easing-gradient-generato.vercel.app/gradient`,
+            destination: `https://ivanovicluka.co/gradient`,
         }
     ];
   }
